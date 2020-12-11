b05498f9114b30f07efb27a0cad88e6994c0d037
William P <kryptonymouspowerplayer@gmail.com>
2020-12-11 14:32:48 -0600

    Removed assertions

--- sorting.py
+++ sorting.py
@@ -4,7 +4,6 @@ class Sorting:
     class Binpacking(object):
         class Bin(object):
             def __init__(self, size: float, buffer: float = 0):
-                assert isinstance(size, (int, float)) and isinstance(buffer, (int, float))
                 self.items  = []
                 self.size   = size
                 self.sum    = 0
@@ -12,11 +11,9 @@ class Sorting:
                 self.full   = False
 
             def doesFit(self, size: float) -> bool:
-                assert isinstance(size, (int, float))
                 return (self.sum + size <= self.size + self.buffer and not self.full) or self.size == -1
 
             def addItem(self, item: object, size: float) -> bool:
-                assert isinstance(item, object) and isinstance(size, (int, float))
                 if not self.doesFit(size = size):
                     return False
                 self.items.append(item)
@@ -29,8 +26,6 @@ class Sorting:
 
         def __init__(self, bins: [float], buffers: [float] = [], 
                 isGreedy: bool = False, doTrash: bool = True):
-            assert isinstance(bins, (list, tuple)) and isinstance(buffers, (list, tuple)) \
-                and isinstance(isGreedy, bool) and isinstance(doTrash, bool)
             Bin             = Sorting.Binpacking.Bin
             self.buffers    = buffers + [0] * max(0, len(bins) - len(buffers))
             self.bins       = [Bin(size = _bin, buffer = buffer) for _bin, buffer in zip(bins, self.buffers)]
@@ -39,28 +34,24 @@ class Sorting:
             self.doTrash    = doTrash
 
         def dump(self, item: object, size: float):
-            assert isinstance(item, object) and isinstance(size, (int, float))
             if self.isGreedy:
                 self.bins.sort(key = lambda x : x.sum, reverse = False)
             for _bin in self.bins:
                 if _bin.addItem(item = item, size = size):
                     return _bin
             if self.doTrash:
-                self.trash.addItem(item = item, size = size)
+                assert self.trash.addItem(item = item, size = size)
             return self.trash
         
         def dumps(self, items: [object], sizes: [float]):
-            assert isinstance(items, (list, tuple)) and isinstance(sizes, (list, tuple))
             sizes += [(0, sizes[0])[len(sizes) == 1]] * max(0, len(items) - len(sizes))
             [self.dump(item = item, size = size) for item, size in zip(items, sizes)]
 
-        def fdumps(self, items: [object], func: 'function'):
-            assert isinstance(func, types.FunctionType) and isinstance(items, (list, tuple))
+        def fdumps(self, items: [object], func: types.FunctionType):
             sizes = [getattr(item, func.__name__)() for item in items]
             [self.dump(item = item, size = size) for item, size in zip(items, sizes)]
 
-        def adumps(self, items: [object], attr: 'attribute'):
-            assert isinstance(attr, str) and isinstance(items, (list, tuple))
+        def adumps(self, items: [object], attr: str):
             sizes = [getattr(item, attr) for item in items]
             [self.dump(item = item, size = size) for item, size in zip(items, sizes)]
 
